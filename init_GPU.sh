91f3f8bc1eacad6bdc634e23e290a75bc22291d1
ofutonMozuku <mizuki@ofuton.net>
2022-08-10 00:29:45 +0900

    GPUをサポート

---
+++ init_GPU.sh
@@ -0,0 +1,15 @@
+#もしconfig.jsonが存在してないなら新規作成
+if [ ! -e config.json ]; then
+
+    #設定ファイルを新規作成
+    touch config.json
+
+    #設定ファイルのテンプレートを書き込み
+    echo -e "{\n    \"prefix\": \">\",\n    \"token\": \"token\"\n}" >> config.json
+fi
+
+#
+rm -rf node_modules
+npm install
+
+docker pull voicevox/voicevox_engine:nvidia-ubuntu20.04-latest
---
+++ run_GPU.sh
@@ -0,0 +1,6 @@
+docker run --name "voicevox" --rm -d --gpus all -p '127.0.0.1:50021:50021' voicevox/voicevox_engine:nvidia-ubuntu20.04-latest
+
+rm -r wav/
+mkdir wav
+
+npx ts-node ./src/main.ts ../config.json
