00f2eb53692c16720776157ebfe16f0a207d8b62
ofutonMozuku <mizuki@ofuton.net>
2022-08-12 01:56:11 +0900

    起動時についでにスラッシュコマンドの登録がされるようにした

--- run.sh
+++ run.sh
@@ -3,4 +3,6 @@ docker run --name "voicevox" --rm -it -d -p '127.0.0.1:50021:50021' voicevox/voi
 rm -r wav/
 mkdir wav
 
+./register.sh
+
 npx ts-node ./src/main.ts ../config.json
--- run_noVoiceVox.sh
+++ run_noVoiceVox.sh
@@ -1,4 +1,6 @@
 rm -r wav/
 mkdir wav
 
+./register.sh
+
 npx ts-node ./src/main.ts ../config.json
